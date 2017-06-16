c2f990deb264e5b9c9a3e599816aa1d8fd817516
sorsusiri <sorussiri@gmail.com>
2017-06-16 23:06:53 +0700

    Update chapter-01.php

--- php/example/chapter-01.php
+++ php/example/chapter-01.php
@@ -2,8 +2,9 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4feee0.....43cff0';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'U3df1cac88e3510a10684dc664237064b';
+$access_token  = 'ifNEIb0T0fu00LwBVddqYSarOl/O+5v8LEhd9BbPNtBBT0mqK6hYGI5SgV4IYxAyN3CAOpcCNaTsf5q+jT9eLNiEa9vgsmPuEUF60+KL41WJ09ck4ZYVu8BfOJVexyb5/qvfajyXkHI2yF3D1QL7hgdB04t89/1O/w1cDnyilFU=
+';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
